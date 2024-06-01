356662e129ffb1f9f7fcfd40db3d189973108795
Adi <me.adishdahal@gmail.com>
2024-06-01 13:53:22 +0545

    changes

--- src/Components/Navbar/RightComp.jsx
+++ src/Components/Navbar/RightComp.jsx
@@ -46,7 +46,7 @@ export default function RightComp() {
         >
           <div className="px-4 py-3" role="none">
             <p className="text-sm text-gray-900 dark:text-white" role="none">
-              Adish Dahal
+              Adish Dahal 
             </p>
             <p
               className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
